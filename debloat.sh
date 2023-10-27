097d7f4b53360560c80cd2fcd860e93ad3ed4c13
SamTheDev <77497287+samthedev32@users.noreply.github.com>
2023-10-27 20:17:49 +0200

    Create debloat.sh

---
+++ debloat.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# debloat-er for MIUI 12.5 (Pine)
+# https://xiaomifirmwareupdater.com/miui/pine/stable/V12.5.3.0.QCMEUXM/
+
+# List connected devices
+adb devices
+
+# List of packages to uninstall
+packages=(
+# Preinstalled Games
+"com.block.puzzle.game.hippo.mi"
+"com.sukhavati.gotoplaying.bubble.BubbleShooter.mint"
+"com.crazy.juicer.xm"
+"com.logame.eliminateintruder3d"
+"com.mintgames.triplecrush.tile.fun"
+
+# Facebook & Services
+"com.facebook.katana"
+"com.facebook.services"
+"com.facebook.system"
+"com.facebook.appmanager"
+
+"com.netflix.mediaclient"
+"com.netflix.partner.activation"
+
+"com.mi.global.bbs"
+"cn.wps.moffice_eng"
+
+# Android / Google Apps
+"com.android.chrome" # i prefer firefox
+"com.google.android.apps.docs" # i don't use you
+"com.google.android.apps.maps" # i luv maps, but i prefer storage
+"com.android.deskclock" # replaced by Google Clock 
+"com.android.soundrecorder" # i'll use Dolby On
+
+"com.google.android.apps.tachyon"
+"com.google.android.videos"
+"com.google.android.music"
+
+# Mi Apps
+"com.miui.calculator" # replaced by Google Calculator
+"com.mi.android.globalFileexplorer" # replaced by Google Files
+
+"com.miui.fm" # who listens to radio anyways?
+"com.miui.gallery" # replaced by Google Photos
+"com.mi.globalbrowser" # we'll have firefox, remember?
+"com.miui.videoplayer" # just use vlc
+"com.miui.player" # can't you afford spotify?
+)
+
+# Uninstall packages using ADB
+for package in "${packages[@]}"; do
+  echo Uninstalling "$package"
+  adb shell pm uninstall "$package"
+  adb shell pm uninstall --user 0 "$package"
+done
